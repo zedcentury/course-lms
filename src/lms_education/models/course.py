bfb6e0e7f97962a2e5d20be69c92c3da5dd8c49f
Asliddin <zedcentury1@gmail.com>
2025-11-12 15:38:11 +0500

    project

--- src/lms_education/models/course.py
+++ src/lms_education/models/course.py
@@ -11,3 +11,21 @@ class Course(models.Model):
     name = fields.Char(string="Name", required=True)
     lesson_ids = fields.One2many("le.lesson", "course_id", string="Lessons")
     subject_ids = fields.Many2many("le.subject", string="Subjects")
+    status = fields.Selection([
+        ("draft", "Draft"),
+        ("open", "Open"),
+        ("closed", "Closed"),
+    ], default="draft", required=True)
+
+    def action_create_group(self):
+        self.ensure_one()
+
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": "le.group",
+            "view_mode": "form",
+            "target": "current",
+            "context": {
+                "default_course_id": self.id,
+            }
+        }
