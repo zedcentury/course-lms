57ef2d0b69fa6e7f055d33d5e6ead8fd3fcdb743
Asliddin <zedcentury1@gmail.com>
2025-11-13 21:00:00 +0500

    ...

--- src/lms_education/models/group.py
+++ src/lms_education/models/group.py
@@ -11,3 +11,14 @@ class Group(models.Model):
     name = fields.Char(string="Name", required=True)
     course_id = fields.Many2one("le.course", string="Course", required=True)
     student_ids = fields.One2many("le.group.student", "group_id", string="Students")
+    schedule_table_ids = fields.One2many("le.schedule.table", "group_id", string="Schedule Tables")
+
+    def action_view_schedule_lessons(self):
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": "le.schedule.lesson",
+            "view_mode": "list,form",
+            "domain": [
+                ("schedule_table_id", "in", self.schedule_table_ids.ids)
+            ]
+        }
