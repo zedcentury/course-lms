9ab689355cab36aea7deb4192ab9059da07dddfa
Asliddin <zedcentury1@gmail.com>
2025-11-18 20:30:02 +0500

    lesson time conflict

--- src/lms_education/models/schedule_lesson.py
+++ src/lms_education/models/schedule_lesson.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ScheduleLesson(models.Model):
@@ -11,3 +11,31 @@ class ScheduleLesson(models.Model):
     lesson_date = fields.Date(string="Lesson Date", required=True)
     lesson_start_time = fields.Float(string="Start Time", required=True)
     lesson_end_time = fields.Float(string="End Time", required=True)
+
+    def get_lessons(self):
+        if not self.id:
+            raise ValueError("ABCD")
+
+        self.name = "Asliddin"
+
+    @api.constrains("lesson_start_time", "lesson_end_time", "lesson_date", "teacher_id")
+    def _check_conflict(self):
+        """
+        Joriy dars kuni joriy o'qituvchiga dars vaqti konflikt bo'lib qolmasligi tekshirildi
+        """
+
+        for record in self:
+            if self.env["le.schedule.lesson"].sudo().search_count([
+                "&",
+                ("lesson_date", "=", record.lesson_date),
+                "&",
+                ("teacher_id", "=", record.teacher_id.id),
+                "|",
+                "&",
+                ("lesson_start_time", "<", record.lesson_start_time),
+                ("lesson_end_time", ">", record.lesson_start_time),
+                "&",
+                ("lesson_start_time", "<", record.lesson_end_time),
+                ("lesson_end_time", ">", record.lesson_end_time),
+            ]) > 0:
+                raise ValueError("Lesson Conflict")
--- src/lms_education/models/schedule_table.py
+++ src/lms_education/models/schedule_table.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ScheduleTable(models.Model):
@@ -12,6 +12,29 @@ class ScheduleTable(models.Model):
     teacher_id = fields.Many2one("res.users", string="Teacher", required=True)
 
     start_date = fields.Date(string="Start Date", required=True)
+    end_date = fields.Date(string="End Date", compute="_compute_end_date")
     weekday_ids = fields.Many2many("common.weekday", string="Weekdays", required=True)
     lesson_start_time = fields.Float(string="Start Time", required=True)
     lesson_end_time = fields.Float(string="End Time", required=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+
+        for record in records:
+            for lesson in record.group_id.course_id.lesson_ids:
+                lesson_date = False
+
+                self.env["le.schedule.lesson"].create({
+                    "name": lesson.name,
+                    "schedule_table_id": record.id,
+                    "lesson_date": lesson_date,
+                    "lesson_start_time": record.lesson_start_time,
+                    "lesson_end_time": record.lesson_end_time,
+                })
+
+        return records
+
+    def _compute_end_date(self):
+        for record in self:
+            record.end_date = record.schedule_lesson_ids.sorted(lambda x: x.lesson_date)[-1].lesson_date
